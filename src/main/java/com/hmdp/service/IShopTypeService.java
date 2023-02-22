9c81c4a1c3a87433d3a97d38289cd1699baec140
DawenzaiST <1700103031@qq.com>
2023-02-22 16:05:06 +0800

    重新跟踪数据

---
+++ src/main/java/com/hmdp/service/IShopTypeService.java
@@ -0,0 +1,13 @@
+package com.hmdp.service;
+
+import com.hmdp.dto.Result;
+import com.hmdp.entity.ShopType;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ *  服务类
+ */
+public interface IShopTypeService extends IService<ShopType> {
+
+    Result queryList();
+}
